9bf059e619e63debdd40c2f32eb089f6792188d9
Drew Erny <dperny@crimson.ua.edu>
2013-04-16 21:48:15 -0500

    added empty shell for heap.py with common operations

---
+++ heap.py
@@ -0,0 +1,24 @@
+class maxheap:
+    def __init__(self):
+        pass
+
+    def create_heap(self):
+        pass
+
+    def heapify(self,array):
+        pass
+
+    def find_max(self):
+        pass
+
+    def delete_max(self):
+        pass
+
+    def increase_key(self):
+        pass
+
+    def insert(self,value):
+        pass
+
+    def merge(self,value):
+        pass
